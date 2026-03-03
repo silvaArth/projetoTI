96d3cf676f9ba9224323cdfe638b6bc124447368
arthurSilva <arthurinacio1502@gmail.com>
2026-03-03 15:35:54 -0300

    refactor: organiza criacao da tabela em funcao criar_tabela

--- database/database.py
+++ database/database.py
@@ -1,16 +1,18 @@
 import sqlite3 as sql
 
-con = sql.connect("database/database.db")
-cursor = con.cursor()
+def criar_tabela():
+    con = sql.connect("database/database.db")
+    cursor = con.cursor()
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS chamados_ti (
-               id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-               nome TEXT NOT NULL,
-               setor TEXT NOT NULL,
-               problema TEXT NOT NULL,
-               descricao TEXT NOT NULL,
-               urgencia TEXT NOT NULL,
-               status TEXT NOT NULL,
-               data_chamado TEXT NOT NULL)
-               """)
-con.commit()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS chamados_ti (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                setor TEXT NOT NULL,
+                problema TEXT NOT NULL,
+                descricao TEXT NOT NULL,
+                urgencia TEXT NOT NULL,
+                status TEXT NOT NULL,
+                data_chamado TEXT NOT NULL)
+                """)
+    con.commit()
+    con.close()
