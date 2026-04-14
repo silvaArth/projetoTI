df62ea68d1b1247432b8de2cb0e361d2c5005936
arthurSilva <arthurinacio1502@gmail.com>
2026-04-14 18:59:03 -0300

    fix: corrige caminho do banco SQLite para funcionar no executável

--- database/database.py
+++ database/database.py
@@ -1,6 +1,16 @@
 import sqlite3 as sql
+import os
+import sys
+
+def get_base_path():
+    if getattr(sys, 'frozen', False):
+        return os.path.dirname(sys.executable)
+    return os.path.dirname(os.path.abspath(__file__))
 
 def criar_tabela():
+    BASE_DIR = get_base_path()
+    db_path = os.path.join(BASE_DIR, "database.db")
+
     con = sql.connect("database/database.db")
     cursor = con.cursor()
 
