aba340434e12704e4098f0dd159bf2aaf3cc150a
arthurSilva <arthurinacio1502@gmail.com>
2026-04-14 21:12:18 -0300

    fix: corrige uso de sys.frozen

--- integrations/trello_service.py
+++ integrations/trello_service.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 def get_base_path():
-    if getattr(sys, 'forzen', False):
+    if getattr(sys, 'frozen', False):
         return os.path.dirname(sys.executable)
     return os.path.dirname(os.path.abspath(__file__))
 
